8ac3cc57df048d38cba93e419a626724bc7c1727
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-07-21 12:59:55 +0530

    LeetCode

---
+++ B.java
@@ -0,0 +1,8 @@
+public class B{
+    public static void main(Strint []args){
+        int arr[] ={1, 2, 34};
+        while(arr.length){
+            System.err.println();
+        }
+    }
+}
---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String []args){
+        String a= "100";
+        System.out.println(Integer.parseInt(a, 2));
+    }
+}
