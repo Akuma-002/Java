37d9c7d4428c85f90c9e84907902fa7af1c8c0da
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-07-22 07:55:10 +0530

    OOPs

--- B.java
+++ B.java
@@ -1,8 +1,14 @@
-public class B{
-    public static void main(Strint []args){
-        int arr[] ={1, 2, 34};
-        while(arr.length){
-            System.err.println();
+class B {
+    public static int reverseBits(int n) {
+        String temp = Integer.toBinaryString(n);
+        StringBuilder sb = new StringBuilder(temp);
+        while(sb.length() <32){
+            sb.insert(0, '0');
         }
+        sb.reverse();
+        return (int)Long.parseLong(sb.toString(), 2);
+    }
+    public static void main(String args[]){
+        System.out.println(reverseBits(43261596));
     }
 }
