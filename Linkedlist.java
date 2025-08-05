52401889ee33101bc1e744a0ec6571455204b4a1
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-05 21:26:09 +0530

    Insert in linked list

--- Linkedlist.java
+++ Linkedlist.java
@@ -1,3 +1,38 @@
 public class Linkedlist {
     
+    public static void main(String args[]){
+        Node ss = new Node();
+        ss.insert(2);
+    }
+}
+
+
+
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+    public Node(){
+
+    }
+    public static Node Head;
+    public static Node Tail;
+    /*Insert */
+    public static void insert(int data){
+        Node Newnode = new Node(data);
+        if(Head == null){
+            Head = Newnode;
+        }
+        Node temp = Head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = Newnode;
+    }
+    //Display
 }
