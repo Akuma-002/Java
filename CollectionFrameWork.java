0c1227b4cba42bbd2c80de1aedb4bb94e7c3cb10
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2025-08-07 21:00:18 +0530

    Linked List Using Collections

---
+++ CollectionFrameWork.java
@@ -0,0 +1,12 @@
+import java.lang.ProcessBuilder.Redirect.Type;
+import java.util.LinkedList;
+
+public class CollectionFrameWork {
+    public static void main(String [] args){
+        LinkedList <Integer> ll = new LinkedList<>();
+        ll.add(2);
+        ll.add(2);
+        ll.add(2);
+        System.out.println(ll.size());
+    }
+}
